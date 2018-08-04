f498993c2efbf5311dda0e9865cfe13b7d346ecb
batkiz <batkiz@outlook.com>
2018-08-04 13:17:22 +0800

    chap1-1 init

---
+++ src/chap1-1/BinarySearch.java
@@ -0,0 +1,44 @@
+/*
+ * @Author: batkiz 
+ * @Date: 2018-08-04 09:19:39 
+ * @Last Modified by: batkiz
+ * @Last Modified time: 2018-08-04 09:25:57
+ */
+import java.util.Arrays;
+import edu.princeton.cs.algs4.*;
+
+/**
+ * BinarySearch
+ */
+public class BinarySearch {
+
+    public static int rank(int key, int[] a)
+    {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid])
+                hi = mid - 1;
+            else if (key > a[mid])
+                lo = mid + 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] whitelist = new In(args[0]).readAllInts();
+        Arrays.sort(whitelist);
+    
+        while (!StdIn.isEmpty())
+        {
+            int key = StdIn.readInt();
+            if (rank(key, whitelist) == -1)
+                StdOut.println(key);
+        }
+    }
+}
---
+++ src/chap1-1/README.md
@@ -0,0 +1,14 @@
+# 1.1 基础编程模型
+[Programming Model](https://algs4.cs.princeton.edu/11model/)
+
+## [BinarySearch](./BinarySearch.java)
+### 注
+因 API 已过时问题，需将书中源码中的 `int[] whitelist = In.readInts(args[0]);` 改成 `int[] whitelist = new In(args[0]).readAllInts();` [^1]
+[^1]: https://neue.v2ex.com/t/301773
+### 测试数据
+https://algs4.cs.princeton.edu/11model/tinyW.txt
+https://algs4.cs.princeton.edu/11model/tinyT.txt
+https://algs4.cs.princeton.edu/11model/largeW.txt
+https://algs4.cs.princeton.edu/11model/largeT.txt
+
+## 练习
