4172743346689ec4a23068f64fd5b6a40b4bcf3a
batkiz <batkiz@outlook.com>
2018-08-12 17:52:39 +0800

    update

---
+++ src/chap1-2/Ex01.java
@@ -0,0 +1,34 @@
+import edu.princeton.cs.algs4.Point2D;
+
+/*
+ * @Author: batkiz 
+ * @Date: 2018-08-12 14:12:06 
+ * @Last Modified by: batkiz
+ * @Last Modified time: 2018-08-12 14:36:18
+ */
+
+/**
+ * Ex01
+ */
+public class Ex01 {
+
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        Point2D[] points = new Point2D[N];
+        for (int i = 0; i < N; i++) {
+            points[i] = new Point2D(Math.random(), Math.random());
+            points[i].draw();
+        }
+        if (N > 1) {
+            double min = points[0].distanceTo(points[1]);
+            for (int i = 0; i < N - 1; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    if (points[i].distanceTo(points[j]) < min) {
+                        min = points[i].distanceTo(points[j]);
+                    }
+                }
+            }
+            System.out.println("Min distance: " + min);
+        }
+    }
+}
