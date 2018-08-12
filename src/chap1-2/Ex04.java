59715b9a5e3ac62cbb3244de101acf7453452978
batkiz <batkiz@outlook.com>
2018-08-12 22:13:11 +0800

    update

---
+++ src/chap1-2/Ex04.java
@@ -0,0 +1,13 @@
+/**
+ * Ex04
+ */
+public class Ex04 {
+
+    public static void main(String[] args) {
+        String string1 = "hello";
+        String string2 = string1;
+        string1 = "world";
+        System.out.println(string1);
+        System.out.println(string2);
+    }
+}
