87de4c8b4f8bca398e9b9178c7e789947197f545
batkiz <batkiz@outlook.com>
2018-08-25 22:11:56 +0800

    update

---
+++ src/chap1-3/Stack.java
@@ -0,0 +1,55 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Stack
+ */
+public class Stack<Item> implements Iterable<Item> {
+
+    private Node first; // 栈顶（最近添加的元素）
+    private int N; // 元素数量
+
+    private class Node {
+        // 定义了节点的嵌套类
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null; // 或： N == 0
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        // 向栈顶添加元素
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+
+    public Item pop() {
+        // 从栈顶删除元素
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+
+    public static void main(String[] args) {
+        Stack<String> s = new Stack<String>();
+
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-"))
+                s.push(item);
+            else if (!s.isEmpty())
+                StdOut.print(s.pop() + " ");
+        }
+        StdOut.println("(" + s.size() + " left on stack)");
+    }
+}
