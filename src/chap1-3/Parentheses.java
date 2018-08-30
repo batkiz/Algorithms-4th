ae68300caad5ac322ef562a64e1ffb91ee5c1f71
batkiz <batkiz@outlook.com>
2018-08-30 21:29:17 +0800

    update

--- src/chap1-3/Parentheses.java
+++ src/chap1-3/Parentheses.java
@@ -6,6 +6,31 @@ import edu.princeton.cs.algs4.StdIn;
 public class Parentheses {
 
     public static void main(String[] args) {
-        String stream = StdIn.readString();
+        String string = StdIn.readString();
+        Stack<String> stack = new Stack<>();
+        String[] inputs = string.split("");
+        for (String input : inputs) {
+            if (input.equals("{") || input.equals("[") || input.equals("(")) {
+                stack.push(input);
+            } else if (!stack.isEmpty()) {
+                if (input.equals("}")) {
+                    if (!stack.pop().equals("{")) {
+                        System.out.println(false);
+                        return;
+                    }
+                } else if (input.equals("]")) {
+                    if (!stack.pop().equals("[")) {
+                        System.out.println(false);
+                        return;
+                    }
+                } else if (input.equals(")")) {
+                    if (!stack.pop().equals("(")) {
+                        System.out.println(false);
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println(stack.isEmpty());
     }
 }
