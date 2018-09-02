3f343f90cf301e92967383abc9a059a5594d61a7
batkiz <batkiz@outlook.com>
2018-09-02 23:01:25 +0800

    update

---
+++ src/chap1-3/InfixToPostfix.java
@@ -0,0 +1,30 @@
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+ * InfixToPostfix
+ */
+public class InfixToPostfix {
+
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            switch (s) {
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                stack.push(s);
+                break;
+            case "(":
+                break;
+            case ")":
+                System.out.print(stack.pop() + " ");
+                break;
+            default:
+                System.out.print(s + " ");
+            }
+        }
+        System.out.println();
+    }
+}
