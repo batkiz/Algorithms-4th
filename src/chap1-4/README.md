3df9a012af8a3c25911d59775e41f490037c6363
batkiz <batkiz@outlook.com>
2018-09-07 11:06:56 +0800

    update

---
+++ src/chap1-4/README.md
@@ -0,0 +1,5 @@
+# 1.4 算法分析
+[Analysis of Algorithms](https://algs4.cs.princeton.edu/14analysis/)
+
+## API 问题
+`ThreeSum.java` 与 `TwoSumFast.java` 中仍需将输入改为 `int[] a = new In(args[0]).readAllInts();`
---
+++ src/chap1-4/TwoSumFast.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.BinarySearch;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * TwoSumFast
+ */
+public class TwoSumFast {
+
+    public static int count(int[] a) {
+        // 计算和为 0 的正数对的数目
+        Arrays.sort(a);
+        int N = a.length;
+        int cnt = 0;
+        for (int i = 0; i < N; i++)
+            if (BinarySearch.rank(-a[i], a) > i)
+                cnt++;
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new In(args[0]).readAllInts();
+        StdOut.println(count(a));
+    }
+}
