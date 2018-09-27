4faffcf9b2e3e0c7e193fa96203bda7311f9dc70
batkiz <batkiz@outlook.com>
2018-09-27 09:35:21 +0800

    update

---
+++ src/chap2-1/Shell.java
@@ -0,0 +1,58 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+
+/**
+ * Shell
+ */
+public class Shell {
+
+    public static void sort(Comparable[] a) {
+        // 将 a[] 按升序排列
+        int N = a.length;
+        int h = 1;
+        while (h < N / 3)
+            h = 3 * h + 1; // 1, 4, 13, 40, 121, 364, 1093, ...
+        while (h >= 1) {
+            // 将数组变为 h 有序
+            for (int i = h; i < N; i++) {
+                // 将 a[i] 插入到 a[i-h], a[i-2*h], a[i-3*h]... 之中
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h)
+                    exch(a, j, j - h);
+            }
+            h = h / 3;
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        // 在单行中打印数组
+        for (int i = 0; i < a.length; i++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        // 测试数组元素是否有序
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i - 1]))
+                return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // 从标准输入读取字符串，将它们排序并输出
+        String[] a = new In().readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
