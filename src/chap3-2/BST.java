70fe97c20476b08e405177da6c43c7ebb8b56b25
batkiz <batkiz@outlook.com>
2019-04-17 23:42:36 +0800

    update

--- src/chap3-2/BST.java
+++ src/chap3-2/BST.java
@@ -65,4 +65,36 @@ public class BST<Key extends Comparable<Key>, Value> {
         x.N = size(x.left) + size(x.right) + 1;
         return x;
     }
+
+    public Key min() {
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null)
+            return x;
+        return min(x.left);
+    }
+
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if (x == null)
+            return null;
+        return x.key;
+    }
+
+    private Node floor(Node x, Key key) {
+        if (x == null)
+            return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0)
+            return x;
+        if (cmp < 0)
+            return floor(x.left, key);
+        Node t = floor(x.right, key);
+        if (t != null)
+            return t;
+        else
+            return x;
+    }
 }
